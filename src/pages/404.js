c40d310b99f14e5c4e4ed2bc80e8fdeb1e4ebf69
CurtisGrayeBabin <curtsocal@gmail.com>
2021-10-13 12:27:11 -0700

    Add Home button to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,6 +11,7 @@ const NotFoundPage = () => {
       <section className={error}>
         <h1>404</h1>
         <p>Oops, this page doesn't exist!</p>
+        <a href="/" aria-label="Back button"><button>Home</button></a>
       </section>
     </Layout>
   )
