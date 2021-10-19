c8be70f254779a383dda81870859482a1e4ec119
CurtisGrayeBabin <curtsocal@gmail.com>
2021-10-18 20:20:00 -0700

    Improve call to action animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,7 +8,6 @@ import {
 const Hero = (props) => {
 
     /* this is the only way I'm able to get animations to work */
-    const callToAction = downArrow+' animate-bounce';
 
     const noWrapStyle = {
         whiteSpace: 'nowrap'
@@ -19,7 +18,7 @@ const Hero = (props) => {
             <h1>I create single page apps & fast, <span style={noWrapStyle}>SEO-friendly</span> websites</h1>
             <h2>Node.js developer | Front-end engineer</h2>
 
-            <svg xmlns="http://www.w3.org/2000/svg" className={callToAction} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className={downArrow} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
             </svg>
         </section>
