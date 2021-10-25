82f5924a8adcfc57f94ca6604f0284b3ddbeb7d4
CurtisGrayeBabin <curtsocal@gmail.com>
2021-10-24 22:53:21 -0700

    Add opacity to navbar blur effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     transitionProperty: ['hover','responsive', 'motion-safe', 'motion-reduce'],
     extend: {
       animation: ['hover', 'focus'],
+      blur: ['hover', 'focus'],
     },
   },
   plugins: [],
