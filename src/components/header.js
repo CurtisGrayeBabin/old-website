519406e5f96338f8d451fdf09ed7975afc60929c
CurtisGrayeBabin <curtsocal@gmail.com>
2021-10-27 19:10:44 -0700

    Make navbar rounded and give white pill background to icon and name

--- src/components/header.js
+++ src/components/header.js
@@ -19,10 +19,12 @@ const Header = (props) => {
         <Headroom>
             <header className={header}>
                 <Link to="/" className={main}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className={svg1} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-                    </svg>
-                    <span style={{'userSelect': 'none'}}>Curtis Babin</span>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className={svg1} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                        </svg>
+                        <span style={{'userSelect': 'none'}}>Curtis Babin</span>
+                    </div>
                 </Link>
 
                 <BiMenuAltRight style={styleForIcons}/>
