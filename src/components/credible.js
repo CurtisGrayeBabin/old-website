a15acac5df27f6f816c2f9a4bab55cd9b8d80eb8
CurtisGrayeBabin <curtsocal@gmail.com>
2021-11-05 20:31:30 -0700

    Add margin and padding to credible (first blue) section

--- src/components/credible.js
+++ src/components/credible.js
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
 import { FaUserGraduate } from 'react-icons/fa';
-import { icon } from '../styles/icons.module.css'
+import { iconWhite } from '../styles/icons.module.css'
 
 const Credible = (props) => {
     return (
         <section>
             {/* decided to use an icon as the symbol for a section header */}
-            <FaUserGraduate className={icon}/> 
+            <FaUserGraduate className={iconWhite}/> 
+            
             
             <StaticImage src="../images/udacity.jpg" alt="My Udacity Nanodegree certificate for completing their Full Stack JavaScript Developer program" />
             <div style={{'padding':'2vh 0 2vh 0'}}></div>
             <StaticImage src="../images/codecademycert.jpg" alt="My Codecademy certificate for completing their Web Development Career Path program" />
             <div style={{'padding':'2vh 0 2vh 0'}}></div>
             <StaticImage src="../images/degree.jpg" alt="My Bachelor of Science degree in Computer Science from University of California, Irvine" />
+            <div style={{'padding':'2vh 0 2vh 0'}}></div>
         </section>
     )
 }   
