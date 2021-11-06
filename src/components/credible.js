f28adf56958cfcc32afc3c46b3893b51c81d7eb6
CurtisGrayeBabin <curtsocal@gmail.com>
2021-11-05 21:25:36 -0700

    Add border and shadow to every cert

--- src/components/credible.js
+++ src/components/credible.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
 import { FaUserGraduate } from 'react-icons/fa';
 import { iconWhite } from '../styles/icons.module.css'
+import { imgBorder } from '../styles/credible.module.css'
 
 const Credible = (props) => {
     return (
@@ -9,13 +10,19 @@ const Credible = (props) => {
             {/* decided to use an icon as the symbol for a section header */}
             <FaUserGraduate className={iconWhite}/> 
             
+        
+            <div className={imgBorder}>
+                <StaticImage src="../images/udacity.jpg" alt="My Udacity Nanodegree certificate for completing their Full Stack JavaScript Developer program" />
+            </div>
             
-            <StaticImage src="../images/udacity.jpg" alt="My Udacity Nanodegree certificate for completing their Full Stack JavaScript Developer program" />
-            <div style={{'padding':'2vh 0 2vh 0'}}></div>
-            <StaticImage src="../images/codecademycert.jpg" alt="My Codecademy certificate for completing their Web Development Career Path program" />
-            <div style={{'padding':'2vh 0 2vh 0'}}></div>
-            <StaticImage src="../images/degree.jpg" alt="My Bachelor of Science degree in Computer Science from University of California, Irvine" />
-            <div style={{'padding':'2vh 0 2vh 0'}}></div>
+            <div className={imgBorder}>
+                <StaticImage src="../images/codecademycert.jpg" alt="My Codecademy certificate for completing their Web Development Career Path program" />
+            </div>
+            
+            <div className={imgBorder}>
+                <StaticImage src="../images/degree.jpg" alt="My Bachelor of Science degree in Computer Science from University of California, Irvine" />
+            </div>
+
         </section>
     )
 }   
