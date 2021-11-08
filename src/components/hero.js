8bc6617ab2febf149ce4434efa804ba0401bb59b
CurtisGrayeBabin <curtsocal@gmail.com>
2021-11-07 23:15:59 -0800

    Make cta a link to portfolio

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ import {
     downArrow,
     arrowContainer
 } from '../styles/hero.module.css'
-
+import { Link } from 'gatsby'
 
 const Hero = (props) => {
 
@@ -12,11 +12,11 @@ const Hero = (props) => {
         <section className={main}>
             <h1>I create single page apps & fast, SEO-friendly websites</h1>
             <h2>Node.js developer</h2>
-            <div className={arrowContainer}>
+            <Link to="#portfolio" className={arrowContainer}>
                 <svg xmlns="http://www.w3.org/2000/svg" className={downArrow} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
                 </svg>
-            </div>
+            </Link>
         </section>
     )
 }   
