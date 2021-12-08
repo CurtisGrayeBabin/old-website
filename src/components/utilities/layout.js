405dea98169212fe780b98f636039965171b8d32
CurtisGrayeBabin <curtsocal@gmail.com>
2021-12-07 19:58:40 -0800

    Prettier for data.js + Meta description update

--- src/components/utilities/layout.js
+++ src/components/utilities/layout.js
@@ -1,36 +1,44 @@
-import * as React from 'react'
-import Navbar from '../navbar/navbar.js'
-import Footer from '../footer/footer.js'
-import { Helmet } from 'react-helmet'
-import { layout, stickToBottom } from '../../global styles/layout.module.css'
+import * as React from "react";
+import Navbar from "../navbar/navbar.js";
+import Footer from "../footer/footer.js";
+import { Helmet } from "react-helmet";
+import { layout, stickToBottom } from "../../global styles/layout.module.css";
 
 // format of each page (not just Home page)
 const Layout = ({ pageTitle, footerStick, children }) => {
   return (
     <>
+      <title>{pageTitle}</title>
+
+      <Helmet>
+        <meta charSet="utf-8" />
         <title>{pageTitle}</title>
+        <meta
+          name="description"
+          content="The personal website of Curtis Babin, a front-end developer, contains his education and portfolio."
+        />
+        <link rel="canonical" href="https://www.curtisbabin.com" />
+        <meta name="author" content="Curtis Babin"></meta>
+        <meta
+          name="keywords"
+          content="Curtis, Babin, front, end, developer, full, stack, software, engineer, JavaScript, Node, react, Udacity, graduate"
+        ></meta>
+        <link
+          rel="manifest"
+          crossorigin="use-credentials"
+          href="/manifest.webmanifest"
+        />
+      </Helmet>
 
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>{pageTitle}</title>
-          <meta name="description" content="The personal website of Curtis Babin, a front-end developer, that contains his education and portfolio" />
-          <link rel="canonical" href="https://www.curtisbabin.com"/>
-          <meta name="author" content="Curtis Babin"></meta>
-          <meta name="keywords" content="Curtis, Babin, front, end, developer, full, stack, software, engineer, JavaScript, Node, react, Udacity, graduate"></meta>
-          <link rel="manifest" crossorigin="use-credentials" href="/manifest.webmanifest" />
-        </Helmet>
-        
-        <Navbar />
-        
-        <main className={layout}>
-          {children}
-        </main>
+      <Navbar />
 
-        <footer className={footerStick ? stickToBottom : ''}>
-          <Footer />
-        </footer>
+      <main className={layout}>{children}</main>
+
+      <footer className={footerStick ? stickToBottom : ""}>
+        <Footer />
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
