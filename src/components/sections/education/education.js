111b7ffba0200249cfe62ad552c8d6fb85e0c612
CurtisGrayeBabin <curtsocal@gmail.com>
2021-12-09 22:34:18 -0800

    Add about my Udacity Nanodegree article blurb

--- src/components/sections/education/education.js
+++ src/components/sections/education/education.js
@@ -3,7 +3,11 @@ import { StaticImage } from "gatsby-plugin-image";
 import { FaUserGraduate } from "react-icons/fa";
 
 import { icon } from "../../../global styles/icons.module.css";
-import { imgBorder, certificateContainer } from "./education.module.css";
+import {
+  imgBorder,
+  certificateContainer,
+  articleInfo,
+} from "./education.module.css";
 import { adaHeader } from "../../../global styles/index.module.css";
 
 const Education = (props) => {
@@ -22,6 +26,28 @@ const Education = (props) => {
             alt="Here is my Udacity Nanodegree certificate for completing their Full Stack JavaScript Developer program"
           />
         </li>
+        <article className={articleInfo}>
+          <h3>About my Udacity Nanodegree</h3>
+          <p>
+            From April to August 2021, I learned and earned the Udacity Full
+            Stack JavaScript Developer Nanodegree! Before this, I had never
+            touched the back-end before. This program lasted four months. Each
+            month there was a major project due. The first project I made was
+            "rapid-image-prototyping-backend"; this script resizes local images
+            for display in the browser. The final project involved combining
+            both a single front-end and single back-end system. It had to be
+            deployed with a few AWS services while utilizing continuous
+            integration with CircleCI. I learned a lot about full-stack software
+            engineering. Feel free to learn more about this program{" "}
+            <a
+              href="https://www.udacity.com/course/full-stack-javascript-developer-nanodegree--nd0067"
+              target="_"
+            >
+              here!
+            </a>
+          </p>
+        </article>
+
         <li className={imgBorder}>
           <StaticImage
             src="../../../images/codecademycert.jpg"
